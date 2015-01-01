fefb84190dc949778a9b22af5a0e4a494704dc93
catacs <catacsdev@gmail.com>
2015-01-01 16:07:39 +0100

    Scaling content with ScaleManager

---
+++ contrib/sync_gh-pages.bat
@@ -0,0 +1,5 @@
+cd ..
+git checkout gh-pages
+git merge master
+git push origin gh-pages
+git checkout master
--- src/Boot.js
+++ src/Boot.js
@@ -5,6 +5,20 @@ Pong.Boot.prototype = {
     },
     create: function() {
         console.log("Boot");
+
+        // Reponsive and centered canvas
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+        this.scale.minWidth = 320;
+        this.scale.minHeight = 200;
+        this.scale.maxWidth = 720;
+        this.scale.maxHeight = 450;
+
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+
+        this.scale.setScreenSize(true);
+
         this.state.start('Preloader');
     }
 };
